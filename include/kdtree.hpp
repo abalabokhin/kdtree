d43d82ce3a197d0378f17a8604ce5c28c0244e38
Andrey Balabokhin <abalabokhin@gmail.com>
2016-10-09 08:44:28 -0400

    added more error checking in tree and tests

--- include/kdtree.hpp
+++ include/kdtree.hpp
@@ -17,14 +17,19 @@ public:
     /// empty c-tor for serialization.
     KDTree() {}
 
-    KDTree(KDPointStorage<T> * aStorage, size_t aMaxNumberOfPointsInLeafNode = 1)
-        : maxNumberOfPointsInLeafNode(aMaxNumberOfPointsInLeafNode)
+    KDTree(KDPointStorage<T> * aStorage, size_t aMaxPointsNumberInLeafNode = 1)
+        : maxPointsNumberInLeafNode(aMaxPointsNumberInLeafNode)
     {
         storage.reset(aStorage);
         root.reset(buildTree(0, storage->size(), 0));
     }
 
+    size_t getDepth() { return depth; }
+
     KDPoint<T> const & findClosestPoint(KDPoint<T> const & p, size_t & closestPointOriginalI) {
+        if (!root || !storage || storage->size() == 0) {
+            throw std::domain_error("tree or points storage is invalid");
+        }
         closestPointOriginalI = findAClosePoint(p, root.get());
         T minSquareDistance = storage->getPointByOriginalI(closestPointOriginalI).
                 squareDistanceToPoint(p);
@@ -79,10 +84,11 @@ private:
 
     IKDTreeNode * buildTree(size_t leftPointsIndicesI, size_t rightPointsIndicesI, size_t levelI)
     {
+        depth = std::max(depth, levelI + 1);
         if (rightPointsIndicesI <= leftPointsIndicesI) {
             throw std::length_error("left index must always be bigger than the right one");
         }
-        if (rightPointsIndicesI - leftPointsIndicesI <= maxNumberOfPointsInLeafNode) {
+        if (rightPointsIndicesI - leftPointsIndicesI <= maxPointsNumberInLeafNode) {
             /// create a leaf node here
             return new KDTreeLeafNode(leftPointsIndicesI, rightPointsIndicesI);
         } else {
@@ -94,23 +100,25 @@ private:
                         levelI
                         );
 
-            /// find a pivot, and create two subtrees
+            /// find a pivot two create two subtrees
             auto pivot = storage->findPivot(
                         leftPointsIndicesI,
                         rightPointsIndicesI,
                         splitingPlaneCoordinateI
                         );
 
+            /// partition points around the pivot and get the index of
             auto middlePointsIndicesI = storage->partition(
                         leftPointsIndicesI,
                         rightPointsIndicesI,
                         splitingPlaneCoordinateI,
                         pivot);
 
+            /// It should no be empty nodes. Thus, if a middle index not in between the left and
+            /// the rigth one, leaf node is created.
             if (middlePointsIndicesI <= leftPointsIndicesI ||
                     middlePointsIndicesI >= rightPointsIndicesI) {
-                throw std::length_error("middle point index must be betwee left and "
-                                        "right point indecis");
+                return new KDTreeLeafNode(leftPointsIndicesI, rightPointsIndicesI);
             }
 
             auto * node = new KDTreeIntermediateNode<T>(splitingPlaneCoordinateI, pivot);
@@ -124,10 +132,11 @@ private:
     friend class boost::serialization::access;
     template <typename Archive>
     void serialize(Archive &ar, const unsigned int version) {
-        ar & maxNumberOfPointsInLeafNode & storage & root;
+        ar & maxPointsNumberInLeafNode & storage & root;
     }
 
-    size_t maxNumberOfPointsInLeafNode = 1;
+    size_t depth = 0;
+    size_t maxPointsNumberInLeafNode = 1;
     std::unique_ptr<KDPointStorage<T>> storage;
     std::unique_ptr<IKDTreeNode> root;
 };
--- tests/test_kdtree.cpp
+++ tests/test_kdtree.cpp
@@ -2,8 +2,13 @@
 #include <kdtree.hpp>
 
 #include <boost/test/unit_test.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 
 #include <random>
+
+BOOST_CLASS_EXPORT(KDTreeIntermediateNode<float>)
+BOOST_CLASS_EXPORT(KDTreeIntermediateNode<double>)
 
 KDPoint<float> generateKDRandomPoint(size_t K,
                                      std::uniform_real_distribution<> & dist,
@@ -33,10 +38,26 @@ size_t findClosestPoint(
     return bestI;
 }
 
+BOOST_AUTO_TEST_CASE( KDPointStorageTest_invalidTree )
+{
+    KDTree<float> tree;
+    size_t i;
+    BOOST_CHECK_EXCEPTION(
+                tree.findClosestPoint(KDPoint<float>({1, 2}), i),
+                std::domain_error, [](std::domain_error const &){return true;});
+}
+
 BOOST_AUTO_TEST_CASE( KDTreeTest_randomGeneratedTreesTest )
 {
     /// test for random 100 points for dims for dims 1 to 5
     /// than test 1000 point to search
+    /// it can crash somewhere, if two points are very close to each other or the same,
+    /// but it is a good test that shown the correctness of the tree.
+
+    /// the tree is also saved and restored
+    /// it is better to have dedicated test and the way to compae trees
+    /// but we don't have the method to compare two trees, so it is a good way to check
+    /// that the restored tree is the correct one
 
     std::random_device rd;
     std::mt19937 e2(rd());
@@ -51,15 +72,78 @@ BOOST_AUTO_TEST_CASE( KDTreeTest_randomGeneratedTreesTest )
         for (int pointsInFinalNode = 1; pointsInFinalNode < 5; ++pointsInFinalNode) {
             KDTree<float> tree(new KDPointStorage<float>(points, dims), pointsInFinalNode);
 
+            std::stringstream ss;
+            {
+                boost::archive::text_oarchive oa{ss};
+                oa << tree;
+            }
+
+            KDTree<float> restoredTree;
+            {
+                boost::archive::text_iarchive ia{ss};
+                ia >> restoredTree;
+            }
+
             for (int j = 0; j < 1000; ++j) {
                 auto p = generateKDRandomPoint(dims, dist, e2);
                 size_t bestPointI1 = 1000;
-                auto closestPoint = tree.findClosestPoint(p, bestPointI1);
+                auto closestPoint = restoredTree.findClosestPoint(p, bestPointI1);
                 size_t bestPointI2 = findClosestPoint(points, p);
 
                 BOOST_CHECK_EQUAL(bestPointI1, bestPointI2);
                 BOOST_CHECK(closestPoint == points[bestPointI2]);
             }
         }
+    }
+}
+
+BOOST_AUTO_TEST_CASE( KDTreeTest_theSamePointsInTree )
+{
+    /// The tree should be correctly created even if it is created from the same points
+    /// that is a very bad case to use tree
+    std::vector<KDPoint<float>> points;
+    KDPoint<float> point({1, 1});
+    /// have 10 the same points
+    for (int i = 0; i < 10; ++i) {
+        points.push_back(point);
+    }
+
+    KDTree<float> tree(new KDPointStorage<float>(points, 2), 2);
+    BOOST_CHECK_EQUAL(tree.getDepth(), 1);
+}
+
+BOOST_AUTO_TEST_CASE( KDTreeTest_differentDepths )
+{
+    std::vector<KDPoint<float>> points;
+    KDPoint<float> point({1, 1});
+    /// add 6 far points
+    points.push_back(KDPoint<float>({1, 2}));
+    points.push_back(KDPoint<float>({2, 3}));
+    points.push_back(KDPoint<float>({3, 4}));
+    points.push_back(KDPoint<float>({4, 3}));
+    points.push_back(KDPoint<float>({3, 2}));
+    points.push_back(KDPoint<float>({2, 1}));
+
+    /// the depth should be around log_2(nPoints / maxPointsNumberInLeafNode),
+    /// but depends on how well we split points
+    /// it is calculated manually for the given points
+    {
+        KDTree<float> tree(new KDPointStorage<float>(points, 2), 1);
+        BOOST_CHECK_EQUAL(tree.getDepth(), 4);
+    }
+
+    {
+        KDTree<float> tree(new KDPointStorage<float>(points, 2), 2);
+        BOOST_CHECK_EQUAL(tree.getDepth(), 3);
+    }
+
+    {
+        KDTree<float> tree(new KDPointStorage<float>(points, 2), 3);
+        BOOST_CHECK_EQUAL(tree.getDepth(), 2);
+    }
+
+    {
+        KDTree<float> tree(new KDPointStorage<float>(points, 2), 6);
+        BOOST_CHECK_EQUAL(tree.getDepth(), 1);
     }
 }
